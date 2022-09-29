700b033bb81264f1ab6414e2b048851a45a74ac1
j-wass <jonahwasserman97@gmail.com>
2022-09-28 21:16:09 -0700

    add cache for mp3 in localstorage

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from io import BytesIO
 import base64
+from pathlib import Path
 
-from fastapi import FastAPI, Response
+from fastapi import FastAPI, Response, HTTPException
 from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi_cache import FastAPICache
@@ -20,16 +21,23 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+class MPEGResponse(Response):
+    media_type = "audio/mpeg"
+
 @app.on_event("startup")
 async def startup():
     FastAPICache.init(InMemoryBackend(), prefix="fastapi-cache")
 
 @app.get("/phrases/{lang}/{difficulty}")
-@cache(expire=86400, coder=JsonCoder)
+@cache(expire=86400, coder=PickleCoder)
 async def phrases(lang, difficulty):
-    return FileResponse(f"assets/{lang}/phrases_{difficulty}.json")
+    path = f"assets/{lang}/phrases_{difficulty}.json"
+    phrase_book_path = Path(path)
+    if phrase_book_path.is_file():
+        return FileResponse(path)
+    raise HTTPException(status_code=404, detail=f"Language/Difficulty not found: {lang}/{difficulty}")
 
-@app.get("/tts/{lang}/{text_base64}.mp3")
+@app.get("/tts/{lang}/{text_base64}", response_class=MPEGResponse)
 @cache(expire=86400, coder=PickleCoder)
 async def index(lang, text_base64):
     text = base64.b64decode(text_base64 + '==').decode("latin-1")  
@@ -41,5 +49,9 @@ async def index(lang, text_base64):
     # Write tts data to bytes, send it back as audio/mpeg.
     mp3_bytes = BytesIO()
     tts.write_to_fp(mp3_bytes)
-    return Response(mp3_bytes.getvalue(), headers={"content-type":"audio/mpeg", "cache-control": "public, max-age:604800"})
+    return Response(
+        base64.b64encode(mp3_bytes.getvalue()),
+        headers={"content-type":"audio/mpeg", "cache-control": "public, max-age:604800"},
+    )
+    # return Response(mp3_bytes.getvalue(), headers={"content-type":"audio/mpeg", "cache-control": "public, max-age:604800"})
 
